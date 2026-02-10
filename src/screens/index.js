e5b410e0af9220809ca2849bc53649f0570665ca
Parker-Lehner <parks2121@icloud.com>
2026-02-09 21:29:41 -0500

    Add screens index for clean exports

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+export { default as OnboardingScreen } from './OnboardingScreen';
+export { default as DashboardScreen } from './DashboardScreen';
+export { default as AddShiftScreen } from './AddShiftScreen';
+export { default as WeekSummaryScreen } from './WeekSummaryScreen';
+export { default as TipOutCalculatorScreen } from './TipOutCalculatorScreen';
+export { default as SettingsScreen } from './SettingsScreen';
